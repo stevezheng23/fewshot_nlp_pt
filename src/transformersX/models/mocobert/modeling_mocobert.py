0dc341bd5d7997fd3290531e1dcd241c2e9d9a3b
Steve Zheng <stevezheng23@gmail.com>
2021-12-21 18:10:59 +0000

    fix mask memory bank update issue in mocobert modeling

--- src/transformersX/models/mocobert/modeling_mocobert.py
+++ src/transformersX/models/mocobert/modeling_mocobert.py
@@ -503,6 +503,7 @@ class MoCoBertForDualPassageEncoder(MoCoBertPreTrainedModel):
     @torch.no_grad()
     def _update_memory_bank(self, embeds, labels):
         embeds = self._all_gather_and_concat(embeds)
+        labels = self._all_gather_and_concat(labels)
         b, p = embeds.size(0), int(self.memory_ptr)
         assert self.memory_size % b == 0
         self.memory_embeds[:,p:p+b] = embeds.T
